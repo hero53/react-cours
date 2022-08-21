05001212556562073c8eeae0f763064126797ee0
hero53 <45401008+hero53@users.noreply.github.com>
2022-08-21 23:39:31 +0000

    component and event

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import image_1 from "./pexels-1.jpg";
+import image_2 from "./pexels-2.jpg";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const firstBook = {
+  Author: " Kiny",
+  Title: "Père riche",
+  Image: image_1,
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    id: 1,
+    author: " Max",
+    title: "Père riche",
+    image: image_1,
+    height: 200,
+    width: 200,
+  },
+  {
+    id: 2,
+    author: " kiny",
+    title: "L'alchimiste",
+    image: image_2,
+    height: 200,
+    width: 200,
+  },
+];
+function BookList() {
+  return (
+    <article className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </article>
+  );
+}
+const Book = ({ image, height, width, title, author, children }) => {
+  const clickHandler = (e) => {
+    alert(e.target);
+  };
+
+  const conplexExanple = (author) => {
+    alert(author);
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        alert(title);
+      }}
+    >
+      <img src={image} alt="" height={height} width={width} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <button onClick={clickHandler}>click ici</button>
+      <button onClick={() => conplexExanple(author)}>click ici</button>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
